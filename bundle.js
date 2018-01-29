ada43e98068f37219f056cc05268d0aaa3082a00
Bence Erős <ebence88@gmail.com>
2018-01-29 21:00:19 +0100

    animations step-by-step work

--- bundle.js
+++ bundle.js
@@ -49553,40 +49553,32 @@ var loader = new ColladaLoader();
 
 
 var camera, scene, renderer;
-var geometry, material, mesh;
- 
-init();
-animate();
+var geometry, material, mesh, mixer, action, playFrom, playUntil;
+var clock = new THREE.Clock(); 
+
+var models = {
+    "knight": "./knight/knight_low_collada.DAE",
+    "scorpid": "./scorpid/scorpid_collada.DAE"
+}
+
+var modelId = document.location.search.substring(1);
+var modelPath = models[modelId] || models.knight;
  
 function init() {
     
-    loader.load( './knight/knight_low_collada.DAE', function ( collada ) {
+    loader.load(modelPath, function ( collada ) {
         console.log(collada)
-					var mesh = collada.scene;
-                    var sc = 0.15;
-                    mesh.scale.set(sc, sc, sc);
-                    mesh.position.z = -0.5;
-					mixer = new THREE.AnimationMixer(mesh);
-					console.log(collada.animations);
-                    mixer.update(0.1);
+        var mesh = collada.scene;
+        var sc = 0.15;
+        mesh.scale.set(sc, sc, sc);
+        mesh.position.z = -0.5;
+        if (modelId === "scorpid") {
+            mesh.rotation.z = Math.PI;
+        }
+        mixer = new THREE.AnimationMixer(mesh);
+        action = mixer.clipAction(collada.animations[0]);
+        scene.add( mesh );
                     
-                    
-					mixer.clipAction(collada.animations[0]).play();
-
-					/*object.traverse( function ( child ) {
-
-						if ( child instanceof THREE.SkinnedMesh ) {
-
-							var clip = THREE.AnimationClip.parseAnimation( child.geometry.animation, child.geometry.bones );
-                            var action = mixer.clipAction( clip, child );
-                            if (action != null)
-                                action.play();
-
-						}
-
-					});*/
-
-					scene.add( mesh );
     });
  
     camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
@@ -49599,14 +49591,64 @@ function init() {
     document.body.appendChild( renderer.domElement );
  
 }
- 
+
 function animate() {
  
     requestAnimationFrame( animate );
  
     renderer.render( scene, camera );
-    if (mixer) mixer.update(0.1)
+    
+    if (mixer && playUntil) {
+        var delta = clock.getDelta();
+        
+        if (clock.elapsedTime >= (playUntil - playFrom)) {
+            console.log("stopping");
+            action.stop();
+            clock = new THREE.Clock(true);
+            mixer.time = action.time = clock.time = playFrom;
+            action.play();
+        }
+        mixer.update(delta);
+    }
 }
+
+var anims = {};
+
+anims.scorpid = {
+    "walk": [0, 72],
+    "strafe left": [72, 120],
+    "strafe right": [120, 168],
+    "attack 1": [168, 220],
+    "death 1": [220, 292],
+    "attack 2": [292, 350],
+    "attack 3": [350, 440],
+    "death 2": [440, 576],
+    "death 3": [580, 650],
+    "idle 1": [660, 760],
+    "idle 2": [761, 849],
+    "gethit 1": [850, 880],
+    "gethit 2": [880, 950],
+    "jump": [951, 1015]
+}
+
+window.playAnim = function(name) {
+    var anim = anims[modelId][name];
+    if (!anim) {
+        console.error("not found", name);
+        return;
+    }
+    var baseFPS=30.0;
+    action.stop();
+    clock = new THREE.Clock(true);
+    action.time = mixer.time = clock.time = playFrom = anim[0] / baseFPS;
+    playUntil = anim[1] / baseFPS;
+    console.log(playFrom, playUntil);
+    action.play();
+    
+}
+
+init();
+animate();
 
 
 /***/ })
